51ee80d39388aa0e82d063672ab897e214672a01
Lucas Moura Gonçalves <92659220+LucasMZ@users.noreply.github.com>
2022-02-15 16:58:35 -0300

    Curriculo
    Javascript

---
+++ curriculo.js
@@ -0,0 +1,14 @@
+function rolarAoTopo(){
+    window.scroll({
+    top: 0,
+    behavior: 'smooth'
+    }
+    )
+}
+function rolarPraBaixo(){
+    window.scroll({
+    top: 1100,
+    behavior: 'smooth'
+    }
+    )
+}
